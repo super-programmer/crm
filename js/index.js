b556f9be9c575a7ac3ead3661522d9f1aaa1b3ff
super-programmer <593655843@qq.com>
2017-11-16 15:10:05 +0800

    init1

---
+++ js/index.js
@@ -0,0 +1,88 @@
+/**
+ * Created by Administrator on 2017-11-16.
+ */
+var $noBtn = $('.c-top-tips_no'),
+    $modalBg = $('.m-modal-bg'),
+    $loginBtn = $('#loginBtn'),
+    $loginName = $('#loginName'),
+    $loginPass = $('#password'),
+    $signBtn = $('#signBtn'),
+    $modal = $('.m-login'),
+    $popNum = $('.userNum'),
+    $signOut = $('#signOut'),
+    $dataList = [
+        {
+            num : 188666,
+            password : "a1234567"
+        },{
+            num : 888,
+            password : "a1234567"
+        },{
+            num : 327555,
+            password : "a1234567"
+        }
+    ]
+/*modal显示隐藏*/
+showModal = function () {
+    $modalBg.toggle();
+    $modal.toggle();
+},
+    /*登录状态*/
+    storeLogin = function(){
+        document.cookie = "lekeUsename=" + $loginName.val();
+        showModal();
+    },
+    /*cookie读取状态*/
+    setState = function(){
+        var num = document.cookie.split('=')[1];
+        if(num){
+            $noBtn.html(num).addClass('c-top-tips_over');
+            $popNum.html(num);
+            $('.c-top-person').show();
+        }else{
+            $noBtn.html("登录/注册").removeClass('c-top-tips_over');
+            $popNum.html(" ");
+            $('.c-top-person').hide();
+        }
+    };
+;
+/*调出modal层*/
+$noBtn.click(function(){
+    if($(this).hasClass('c-top-tips_over')){
+        return;
+    }
+    showModal();
+})
+$loginBtn.click(function(){
+    var flag = false;
+    if($loginName.val() && $loginPass.val()){
+        for(var i in $dataList){
+            if($loginName.val() == $dataList[i].num && $loginPass.val() == $dataList[i].password){
+                flag = true;
+                storeLogin();
+                setState();
+            }
+        }
+        if(!flag){
+            alert('账号未注册或密码错误！')
+        }
+    }else if($loginName.val()){
+        alert('密码为空！')
+    }else{
+        alert('账号为空！')
+    }
+})
+$signBtn.click(function(){
+    if($loginName.val()){
+        alert('此账号已注册！')
+    }else{
+        alert('请输入账号！')
+    }
+
+})
+/*退出*/
+$signOut.click(function(){
+    document.cookie = "lekeUsename=";
+    setState();
+})
+setState();
